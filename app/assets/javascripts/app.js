694f972e968d650be73b351aef4b36178ccaae07
Andrew Clark <eyepenn@yahoo.com>
2016-11-13 14:12:45 +0000

    Changed pane filter to ng-show for delete to work properly

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,13 +4,11 @@ angular.module('house', ['ngResource', 'ngRoute', 'ngMessages'])
 
 	.controller('ThingsListController', ['$scope', '$resource', function ($scope, $resource) {
 		var Thing = $resource('/things/:id', { id:'@id' });
- 		 $scope.things = Thing.query();
+ 		 
+     $scope.things = Thing.query();
  		 
  		 $scope.submit = function() {
 			Thing.save({ item: $scope.item, room: $scope.room }, function(data) {
-            // console.log(data.id);
-            // console.log(data.item);
-            // console.log(data.room);
       			$scope.things.push({id:data.id, item:$scope.item, room:$scope.room});
       			$scope.message ='Added!';
             $scope.item={};
@@ -20,21 +18,21 @@ angular.module('house', ['ngResource', 'ngRoute', 'ngMessages'])
     		});
        	 };
 
-       	$scope.remove = function(thing, $index) {
-	 			// console.log(thing.id);
-     //    console.log(thing.item);
-     //    console.log(thing.room);
-	 			console.log($index);
+     $scope.remove = function(thing, $index) {
 			 Thing.delete({ id: thing.id }, function() {
+         console.log(thing.id);
+         console.log(thing.item);
+         console.log(thing.room);
+         console.log($index);
 				 $scope.things.splice($index, 1);
 				 $scope.message ='Deleted!';
-     	   	   });
-        };
+     	  });
+      };
 
        	$scope.search = function(thing){
-    		if (!$scope.query || (thing.item.toLowerCase().indexOf($scope.query) != -1) || (thing.room.toLowerCase().indexOf($scope.query.toLowerCase()) != -1) ){
+    		  if (!$scope.query || (thing.item.toLowerCase().indexOf($scope.query) != -1) || (thing.room.toLowerCase().indexOf($scope.query.toLowerCase()) != -1) ){
          		return true;
-   		 	}
+   		 	   }
     	 		  return false;
         };
 
